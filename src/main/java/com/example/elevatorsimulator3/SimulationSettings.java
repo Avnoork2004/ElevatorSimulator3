bd5393be4bd609c4f0af60fc59eed04d3d050d77
avnoor <avnoork2004@gmail.com>
2023-10-31 23:20:13 -0400

    Phase 2

---
+++ src/main/java/com/example/elevatorsimulator3/SimulationSettings.java
@@ -0,0 +1,28 @@
+package com.example.elevatorsimulator3;
+
+import java.util.ArrayList;
+
+public class SimulationSettings {
+    public int floors;
+    public ArrayList<AddPassenger> add_passenger = new ArrayList<>();
+    public ArrayList<PassengerRequestPercentage> passenger_request_percentage = new ArrayList<>();
+    public int number_of_elevators;
+    public int run_simulation;
+
+    // Method to add passengers
+    public void addPassenger(int floor, int startFloor, int endFloor, String passengerType, int quantity) {
+        AddPassenger newPassenger = new AddPassenger(floor, startFloor, endFloor, passengerType, quantity);
+        add_passenger.add(newPassenger);
+    }
+
+//More code to be typed based on the classes, not finished
+
+
+
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
